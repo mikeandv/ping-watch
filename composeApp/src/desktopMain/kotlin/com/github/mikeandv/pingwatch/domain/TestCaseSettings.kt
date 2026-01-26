c946c1ae1c84ae72050677d4bc4bffa4b6b7411f
Mike Andreev <mikeandreev@icloud.com>
2026-01-26 20:12:33 +0100

    configure okhttpClient for parallelism

--- composeApp/src/desktopMain/kotlin/com/github/mikeandv/pingwatch/domain/TestCaseSettings.kt
+++ composeApp/src/desktopMain/kotlin/com/github/mikeandv/pingwatch/domain/TestCaseSettings.kt
@@ -3,6 +3,7 @@ package com.github.mikeandv.pingwatch.domain
 import com.github.mikeandv.pingwatch.listener.TimingEventListenerFactory
 import com.github.mikeandv.pingwatch.result.UrlAvgAggregator
 import okhttp3.ConnectionPool
+import okhttp3.Dispatcher
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 
@@ -17,12 +18,20 @@ class TestCaseSettings private constructor(
         val DEFAULT_FILE_EXTENSIONS = listOf("txt")
         const val DEFAULT_MAX_FILE_SIZE = 5
         const val DEFAULT_MAX_LINE_LIMIT = 20
+        const val DEFAULT_MAX_REQUESTS = 256
+        const val DEFAULT_MAX_REQUESTS_PER_HOST = 256
 
         fun createDefaultSettings(): TestCaseSettings {
             val agg = UrlAvgAggregator()
 
+            val dispatcher = Dispatcher().apply {
+                maxRequests = DEFAULT_MAX_REQUESTS
+                maxRequestsPerHost = DEFAULT_MAX_REQUESTS_PER_HOST
+            }
+
             val client = OkHttpClient.Builder()
                 .cache(null)
+                .dispatcher(dispatcher)
                 .connectionPool(ConnectionPool(0, 1, TimeUnit.NANOSECONDS))
                 .eventListenerFactory(TimingEventListenerFactory { agg.add(it) })
                 .build()
